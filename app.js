123efbf24d2e4eb34c64916ad82fa5a37607f0a3
GXP150 <79392623+gxp150@users.noreply.github.com>
2021-08-01 09:50:36 +1000

    Add files via upload
    SIT120 Assessment 1 - Responsive Web Application Project Proposal
    Create a business idea that could be improved by creating a responsive web app, prepare a short business proposal
    Create the web application using HTML, CSS, Javascript and Vue (Vue is yet to be implemented in Stage 1)

---
+++ app.js
@@ -0,0 +1,8 @@
+Vue.compenent("greeting", {
+    template: "<H1> Welcome to coligo!</H1>"
+});
+
+// create a new Vue instance and mount it to our div element above with the id of app
+var vm = new Vue({
+    al: "#app'"
+});
